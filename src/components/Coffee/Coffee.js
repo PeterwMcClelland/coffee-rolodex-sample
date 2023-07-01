906a20c223aff93a3bbfdf2809d3530ebd1dd2ea
Peter McClelland <peterwmcclelland@gmail.com>
2023-07-01 17:29:39 -0500

    code cleaning

--- src/components/Coffee/Coffee.js
+++ src/components/Coffee/Coffee.js
@@ -1,45 +1,56 @@
-import { Button } from '@mui/material';
-import React, { useState} from 'react';
+import { Button } from "@mui/material";
+import React, { useState } from "react";
 import "./Coffee.css";
-import ScrollTrigger from 'react-scroll-trigger'
-import { Link } from 'react-router-dom';
+import ScrollTrigger from "react-scroll-trigger";
+import { Link } from "react-router-dom";
 
 const Coffee = (props) => {
-
-    const [animation, setAnimation] = useState(false)
+  const [animation, setAnimation] = useState(false);
 
   const onEnterViewport = () => {
-    setAnimation(true)
-  }
-    const { _id, name, brand, espresso_gs, output, time, tds, percent, country, image} = props.coffee;
+    setAnimation(true);
+  };
+  const {
+    _id,
+    name,
+    brand,
+    espresso_gs,
+    output,
+    time,
+    tds,
+    percent,
+    country,
+    image,
+  } = props.coffee;
 
-    return (
-        
-        <div className={`card ${animation ? 'slideFromRight' : ''}`}>
-            <ScrollTrigger onEnter={onEnterViewport}></ScrollTrigger>
-            <div className='column'>
-                <h1 className='brand-txt'>{brand}</h1>
-                <h2 className='name-txt'>{name}</h2>
-                <img src={image} alt={name} width={500} />
-            </div>
-            
-            
-            <div className='column'>
-                <ul >
-                    <li>Notes: {country}</li>
-                    <li>Input: {espresso_gs}g</li>
-                    <li>Output: {output}g</li>
-                    <li>Time: {time}s</li>
-                    <li>TDS: {tds}</li>
-                    <li>Percent: {percent}%</li>
-                    
-                </ul>
-            </div>
-            
-            <Button component={Link} to={`/coffees/${_id}`}>Update</Button>
-            <Button className='delete-button'  onClick={props.deleteHandler}>Delete</Button>
-        </div>
-    );
+  return (
+    <div className={`card ${animation ? "slideFromRight" : ""}`}>
+      <ScrollTrigger onEnter={onEnterViewport}></ScrollTrigger>
+      <div className="column">
+        <h1 className="brand-txt">{brand}</h1>
+        <h2 className="name-txt">{name}</h2>
+        <img src={image} alt={name} width={500} />
+      </div>
+
+      <div className="column">
+        <ul>
+          <li>Notes: {country}</li>
+          <li>Input: {espresso_gs}g</li>
+          <li>Output: {output}g</li>
+          <li>Time: {time}s</li>
+          <li>TDS: {tds}</li>
+          <li>Percent: {percent}%</li>
+        </ul>
+      </div>
+
+      <Button component={Link} to={`/coffees/${_id}`}>
+        Update
+      </Button>
+      <Button className="delete-button" onClick={props.deleteHandler}>
+        Delete
+      </Button>
+    </div>
+  );
 };
 
 export default Coffee;
