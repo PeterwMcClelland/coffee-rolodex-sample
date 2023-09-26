314262ade34d91ab693be2c0610d437c9d172867
Peter McClelland <peterwmcclelland@gmail.com>
2023-09-25 21:35:57 -0500

    code cleaning

--- src/components/AddCoffee.js
+++ src/components/AddCoffee.js
@@ -80,131 +80,130 @@ const AddCoffee = () => {
         className={`addcoffee ${animation ? "liftIntoPlace-addcoffee" : ""}`}
         onSubmit={handleSubmit}
       >
-      <Box
-        display="flex"
-        flexDirection="column"
-        justifyContent={"center"}
-        maxWidth={700}
-        alignContent={"center"}
-        alignSelf={"center"}
-        marginLeft={"auto"}
-        marginRight={"auto"}
-        marginTop={10}
-        marginBottom={10}
-      >
-        <FormLabel>Coffee</FormLabel>
-        <TextField
-          value={inputs.name}
-          onChange={handleChange}
-          margin="normal"
-          fullWidth
-          variant="outlined"
-          name="name"
-          placeholder='Example "NAMBALLE - Cajamarca, Peru"'
-        />
+        <Box
+          display="flex"
+          flexDirection="column"
+          justifyContent={"center"}
+          maxWidth={700}
+          alignContent={"center"}
+          alignSelf={"center"}
+          marginLeft={"auto"}
+          marginRight={"auto"}
+          marginTop={10}
+          marginBottom={10}
+        >
+          <FormLabel>Coffee</FormLabel>
+          <TextField
+            value={inputs.name}
+            onChange={handleChange}
+            margin="normal"
+            fullWidth
+            variant="outlined"
+            name="name"
+            placeholder='Example "NAMBALLE - Cajamarca, Peru"'
+          />
 
-        <FormLabel>Roaster</FormLabel>
-        <TextField
-          value={inputs.brand}
-          onChange={handleChange}
-          margin="normal"
-          fullWidth
-          variant="outlined"
-          name="brand"
-          placeholder='Example "Sweet Bloom"'
-        />
+          <FormLabel>Roaster</FormLabel>
+          <TextField
+            value={inputs.brand}
+            onChange={handleChange}
+            margin="normal"
+            fullWidth
+            variant="outlined"
+            name="brand"
+            placeholder='Example "Sweet Bloom"'
+          />
 
-        <FormLabel>Grams In</FormLabel>
-        <TextField
-          value={inputs.espresso_gs}
-          onChange={handleChange}
-          margin="normal"
-          fullWidth
-          variant="outlined"
-          name="espresso_gs"
-          placeholder="Grams of Espresso"
-          inputProps={{ inputMode: "decimal", pattern: "[0-9]*([.][0-9]+)?" }}
-        />
+          <FormLabel>Grams In</FormLabel>
+          <TextField
+            value={inputs.espresso_gs}
+            onChange={handleChange}
+            margin="normal"
+            fullWidth
+            variant="outlined"
+            name="espresso_gs"
+            placeholder="Grams of Espresso"
+            inputProps={{ inputMode: "decimal", pattern: "[0-9]*([.][0-9]+)?" }}
+          />
 
-        <FormLabel>Grams Out</FormLabel>
-        <TextField
-          value={inputs.output}
-          onChange={handleChange}
-          margin="normal"
-          fullWidth
-          variant="outlined"
-          name="output"
-          placeholder="Grams of Liquid"
-          inputProps={{ inputMode: "decimal", pattern: "[0-9]*([.][0-9]+)?" }}
-        />
+          <FormLabel>Grams Out</FormLabel>
+          <TextField
+            value={inputs.output}
+            onChange={handleChange}
+            margin="normal"
+            fullWidth
+            variant="outlined"
+            name="output"
+            placeholder="Grams of Liquid"
+            inputProps={{ inputMode: "decimal", pattern: "[0-9]*([.][0-9]+)?" }}
+          />
 
-        <FormLabel>Time</FormLabel>
-        <TextField
-          value={inputs.time}
-          onChange={handleChange}
-          margin="normal"
-          fullWidth
-          variant="outlined"
-          name="time"
-          placeholder="Number of Seconds"
-          inputProps={{ inputMode: "decimal", pattern: "[0-9]*([.][0-9]+)?" }}
-        />
+          <FormLabel>Time</FormLabel>
+          <TextField
+            value={inputs.time}
+            onChange={handleChange}
+            margin="normal"
+            fullWidth
+            variant="outlined"
+            name="time"
+            placeholder="Number of Seconds"
+            inputProps={{ inputMode: "decimal", pattern: "[0-9]*([.][0-9]+)?" }}
+          />
 
-        <FormLabel>TDS</FormLabel>
-        <TextField
-          value={inputs.tds}
-          onChange={handleChange}
-          margin="normal"
-          fullWidth
-          variant="outlined"
-          name="tds"
-          placeholder='TDS Results "9.24"'
-          inputProps={{ inputMode: "decimal", pattern: "[0-9]*([.][0-9]+)?" }}
-        />
+          <FormLabel>TDS</FormLabel>
+          <TextField
+            value={inputs.tds}
+            onChange={handleChange}
+            margin="normal"
+            fullWidth
+            variant="outlined"
+            name="tds"
+            placeholder='TDS Results "9.24"'
+            inputProps={{ inputMode: "decimal", pattern: "[0-9]*([.][0-9]+)?" }}
+          />
 
-        <FormLabel>Percent</FormLabel>
-        <TextField
-          value={inputs.percent}
-          onChange={handleChange}
-          margin="normal"
-          fullWidth
-          variant="outlined"
-          name="percent"
-          placeholder="AutoFill %"
-        />
+          <FormLabel>Percent</FormLabel>
+          <TextField
+            value={inputs.percent}
+            onChange={handleChange}
+            margin="normal"
+            fullWidth
+            variant="outlined"
+            name="percent"
+            placeholder="AutoFill %"
+          />
 
-        <FormLabel>Image</FormLabel>
-        <TextField
-          value={inputs.image}
-          onChange={handleChange}
-          margin="normal"
-          fullWidth
-          variant="outlined"
-          name="image"
-          placeholder='Paste "Copy Image Address" Here'
-        />
+          <FormLabel>Image</FormLabel>
+          <TextField
+            value={inputs.image}
+            onChange={handleChange}
+            margin="normal"
+            fullWidth
+            variant="outlined"
+            name="image"
+            placeholder='Paste "Copy Image Address" Here'
+          />
 
-        <FormLabel>Notes</FormLabel>
-        <TextField
-          value={inputs.country}
-          onChange={handleChange}
-          margin="normal"
-          fullWidth
-          variant="outlined"
-          name="country"
-          multiline
-          maxRows={4}
-          placeholder="Tasting Notes"
-        />
+          <FormLabel>Notes</FormLabel>
+          <TextField
+            value={inputs.country}
+            onChange={handleChange}
+            margin="normal"
+            fullWidth
+            variant="outlined"
+            name="country"
+            multiline
+            maxRows={4}
+            placeholder="Tasting Notes"
+          />
 
-        <Button variant="contained" type="submit">
-          Add Coffee
-        </Button>
-      </Box>
-    </form>
+          <Button variant="contained" type="submit">
+            Add Coffee
+          </Button>
+        </Box>
+      </form>
     </ScrollTrigger>
   );
-    
 };
 
 export default AddCoffee;
